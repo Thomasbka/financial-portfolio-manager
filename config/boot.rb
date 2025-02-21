61c6b3f03a697479c59d4b24cfa292c6033e0174
Thomas <163157813+Thomasbka@users.noreply.github.com>
2025-02-21 14:13:23 +0100

    Configure buildpacks and logger loading

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,10 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+require "logger"
 
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
+
+if ENV['RAILS_ENV'] != 'production'
+  require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+end
+
 #require "bootsnap/setup" # Speed up boot time by caching expensive operations.
