9eef25f202913f2fcaf9d7089678fc3244c3f556
Thomas <163157813+Thomasbka@users.noreply.github.com>
2025-03-03 17:06:29 +0100

    Switched to a streamlined single-pack React + Reails setup.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,4 @@
 class StaticPagesController < ApplicationController
   def dashboard
   end
-
-  def portfolio
-  end
-
-  def analyser
-  end
-
-  def tracker
-  end
-
-  def history
-  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,43 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 Rails.start()
 ActiveStorage.start()
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Dashboard from "@src/dashboard.jsx";
+import Analyser from "@src/analyser.jsx";
+import History from "@src/history.jsx";
+import Portfolio from "@src/portfolio.jsx";
+import Tracker from "@src/tracker.jsx";
+
+import "bootstrap"; 
+import "bootstrap/dist/css/bootstrap.css";
+import "../src/dashboard.scss";
+import "../src/analyser.scss";
+import "../src/history.scss";
+import "../src/portfolio.scss";
+import "../src/tracker.scss";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        {/* Use 'exact' on the root route so it doesn't also match subpaths */}
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/analyser" component={Analyser} />
+        <Route path="/history" component={History} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/tracker" component={Tracker} />
+      </Switch>
+    </Router>
+  );
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
